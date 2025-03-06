02c6402491a4aefb2f6f39580585369abe61b1c1
Carolina Praxedes <ana_carolina47@hotmail.com>
2025-03-05 23:13:56 -0300

    funçao de adicionar e atualizar lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = []
+let nomeAmigo;
+let listaAmigos = document.getElementById('listaAmigos')
+
+function adicionarAmigo(){
+    nomeAmigo = document.getElementById('amigo').value
+    if (nomeAmigo == ''){
+        alert('Por favor, insira um nome.')
+    }else{
+        amigos.push(nomeAmigo)
+    }
+    document.getElementById('amigo').value = '';
+    exibirLista();
+}
+
+function capitalizarPrimeiraLetra(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function exibirLista(){
+    listaAmigos.innerHTML = '';
+    amigos.map((amigo,index)=>{
+        let itemLista = document.createElement('li');
+        itemLista.textContent =  capitalizarPrimeiraLetra(amigo);
+        listaAmigos.appendChild(itemLista);
+    })
+}
